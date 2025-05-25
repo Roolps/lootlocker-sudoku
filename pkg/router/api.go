8caf02a3601e685666243e92dbc2fe0a2cfc4fe5
Roolps <yara@clubnode.com>
2025-05-25 16:01:33 +0200

    moved the state api to its own file
    Signed-off-by: Roolps <yara@clubnode.com>

--- pkg/router/api.go
+++ pkg/router/api.go
@@ -4,9 +4,6 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"time"
-
-	"github.com/roolps/lootlocker-sudoku/backend/pkg/lootlocker"
 )
 
 type apiresponse struct {
@@ -63,72 +60,4 @@ func statusnotacceptable(msg string) *apiresponse {
 
 func statusok(data any) *apiresponse {
 	return &apiresponse{Code: http.StatusOK, Message: "success", Data: data}
-}
-
-type stateEndpoint struct {
-}
-
-func (e *stateEndpoint) Get(s *session, w http.ResponseWriter) *apiresponse {
-	metadata, err := lootlockerClient.ListMetadata(lootlocker.MetadataSourcePlayer, s.PlayerID, s.Token)
-	if err != nil {
-		if err == lootlocker.ErrForbidden {
-			// return a 403 error as the login token is no longer valid
-			c := &http.Cookie{
-				Name:     "session",
-				Value:    "",
-				Path:     "/",
-				Domain:   origin,
-				Expires:  time.Unix(0, 0),
-				MaxAge:   -1,
-				Secure:   true,
-				HttpOnly: true,
-				SameSite: http.SameSiteLaxMode,
-			}
-			http.SetCookie(w, c)
-
-			return statusForbidden(err.Error())
-		}
-		return statusinternalservererror(err.Error())
-	}
-	if gameState, ok := metadata["current_state"]; ok {
-		return statusok(gameState.Value)
-	}
-
-	// empty game :)
-	val := make([][]map[string]interface{}, 9)
-	for i := range val {
-		val[i] = make([]map[string]interface{}, 9)
-		for j := range val[i] {
-			val[i][j] = map[string]interface{}{}
-		}
-	}
-	return statusok(val)
-}
-
-func (e *stateEndpoint) Post(s *session, w http.ResponseWriter, raw []byte) *apiresponse {
-	var gameState [][]cell
-	if err := json.Unmarshal(raw, &gameState); err != nil {
-		return statusinternalservererror(err.Error())
-	}
-	if err := lootlockerClient.UpdatePlayerMetadata(s.Token, []lootlocker.Metadata{{
-		Access: []string{
-			"game_api.read",
-			"game_api.write",
-		},
-		Key:    "current_state",
-		Tags:   []string{},
-		Value:  gameState,
-		Type:   lootlocker.MetadataTypeJSON,
-		Action: lootlocker.MetadataActionUpdate,
-	}}); err != nil {
-		logger.Error(err)
-		return statusinternalservererror(err.Error())
-	}
-	return statusok(nil)
-}
-
-type cell struct {
-	Value     *int   `json:"value,omitempty"`
-	Immutable bool   `json:"immutable,omitempty"`
-	Pencil    []bool `json:"pencil,omitempty"`
 }
